589931688a5019256e023f09615e29f34099ce18
JPoitreau <113617948+JPoitreau@users.noreply.github.com>
2026-07-09 21:47:24 +0200

    api take new values from the user and return a prediction

--- src/api/scoring_api.py
+++ src/api/scoring_api.py
@@ -3,22 +3,33 @@ import pandas as pd
 import numpy as np
 from pathlib import Path
 from pydantic import TypeAdapter, ValidationError
+from pickle import load
+import __main__
+
+from src.utils.utils import custom_sampler_ratio, business_cost
+setattr(__main__, "custom_sampler_ratio", custom_sampler_ratio)
+setattr(__main__, "business_cost", business_cost)
 
 BASE_DIR = Path(__file__).resolve().parents[2]
 DATA_PATH = BASE_DIR / "data" / "original" / "demonstration_data.csv"
 SCHEMA_PATH = BASE_DIR / "data" / "schema" / "typeAdapters.json"
+MODEL_PATH = BASE_DIR / "src" / "model" / "lgb_model.pkl"
 
 data = pd.read_csv(DATA_PATH, sep = ";")
 features_list = list(data.columns)
 
 explicative_features = pd.read_json(SCHEMA_PATH, typ='series')
+explicative_features_list = list(explicative_features.index)
 
-features_list_shortened = list(explicative_features.index)
-
+with open(MODEL_PATH, "rb") as f:
+    scoring_model = load(f)
 
 def update_table(features: list[str] | str | None):
     """
     Get list of features name and convert it as an empty dataframe.
+
+    Args:
+    features: list of features selected by the user
     """
     if not features:
         return pd.DataFrame(
@@ -37,6 +48,13 @@ def update_table(features: list[str] | str | None):
 
 def validate_params(df: pd.DataFrame,
                     types : pd.Series):
+    """
+    Validate parameters type using pydantic.
+
+    Args:
+    df: 2 columns feature-value
+    types: series with features as index giving awaited types
+    """
     errors = []
     parsed = {}
 
@@ -60,11 +78,25 @@ def validate_params(df: pd.DataFrame,
 
     return "✅ Paramètres valides.", parsed
 
+def infer_from_new_vector(params: dict):
+    """
+    Create new vector from given parameters. Missing parameters are filled
+    with nan values and imputed in the model pipeline.
+
+    Args:
+    params: dictionnary of features and keys with new values.
+    """
+    new_vector = (pd.DataFrame([params], index=[0])
+                  .reindex(columns=data.columns, fill_value=np.nan))
+    prediction = scoring_model.predict(new_vector)
+    return prediction.tolist()
+
+
 
 with gr.Blocks() as demo:
 
     choosed_features = gr.Dropdown(
-        choices = features_list_shortened,
+        choices = explicative_features_list,
         multiselect = True,
         value = list
     )
@@ -93,8 +125,16 @@ with gr.Blocks() as demo:
         fn=validate_params,
         inputs=[user_table, types_state],
         outputs=[status, validated_params],
-        api_name="validate_params",
     )
+
+    predict_button = gr.Button("Obtenir les prédictions du modèle")
+    prediction = gr.JSON()   
+
+    predict_button.click(
+        fn=infer_from_new_vector,
+        inputs=validated_params,
+        outputs=prediction
+    )                           
 
 
 demo.launch()
