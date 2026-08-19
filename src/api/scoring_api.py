339fd357e08bc701aa20c7d21ab943fa80289a96
JPoitreau <113617948+JPoitreau@users.noreply.github.com>
2026-08-19 17:15:35 +0200

    Import business_cost and custom_sampler_ratio functions

--- src/api/scoring_api.py
+++ src/api/scoring_api.py
@@ -11,6 +11,7 @@ from pydantic import TypeAdapter, ValidationError
 from sklearn.pipeline import Pipeline
 
 from src.database.database import save_error_log, save_prediction_log
+from src.utils.utils import business_cost, custom_sampler_ratio  # noqa: F401
 
 #Commande de lancement du script: python -m src.api.scoring_api
 #En local, enregistrement sur csv plutôt que sur PostgreSQL: $env:APP_ENV="remote"
